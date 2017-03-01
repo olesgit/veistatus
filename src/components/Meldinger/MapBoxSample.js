a8394a973d068a4ee85ab994164da83e9bdd4015
olesgit <ole.petter.haugen@bym.oslo.kommune.no>
2017-03-01 08:33:37 +0100

    updated tilelayer style

--- src/components/Meldinger/MapBoxSample.js
+++ src/components/Meldinger/MapBoxSample.js
@@ -8,6 +8,20 @@ const markers = [
   {lat: 59.9445, lng: 10.77},
   {lat: 59.9467, lng: 10.77}
 ];
+
+//TileLayer settings:
+//orginal sample:
+// url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+// attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// as from barnehager i oslo:
+// L.tileLayer('https://api.mapbox.com/styles/v1/webforvaltningen/cirlr93tl0010gyly7o56ugi3/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2ViZm9ydmFsdG5pbmdlbiIsImEiOiJjaXJsczQ5dnAwMDMxaG5rd2xnNGt2MGZvIn0.BGtT-dezZ_5hseqXmkMAoQ', {
+//         attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//         maxZoom: 18,
+//         minZoom: 7,
+//         detectRetina: true
+
+
+
 export class MapView extends React.Component {
     constructor(props) {
         super(props)
@@ -20,7 +34,7 @@ export class MapView extends React.Component {
                     center={position}
                     zoom={10}>
                     <TileLayer
-                        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                        url='https://api.mapbox.com/styles/v1/webforvaltningen/cirlr93tl0010gyly7o56ugi3/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2ViZm9ydmFsdG5pbmdlbiIsImEiOiJjaXJsczQ5dnAwMDMxaG5rd2xnNGt2MGZvIn0.BGtT-dezZ_5hseqXmkMAoQ'
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
 
