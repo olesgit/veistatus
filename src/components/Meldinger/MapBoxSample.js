87036150377d8c087ade130d7c2f149b623c3b91
olesgit <ole.petter.haugen@bym.oslo.kommune.no>
2017-03-02 07:59:53 +0100

    updated mapboxsample

--- src/components/Meldinger/MapBoxSample.js
+++ src/components/Meldinger/MapBoxSample.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Map, TileLayer } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-markercluster';
+
 const position = [59.94, 10.77]
 const markers = [
   {lat: 59.9412, lng: 10.77},
   {lat: 59.9445, lng: 10.77},
   {lat: 59.9467, lng: 10.77}
 ];
+var nominatim = require('nominatim-client');
 
 //TileLayer settings:
 //orginal sample:
@@ -21,18 +23,49 @@ const markers = [
 //         detectRetina: true
 
 
+// Set the global settings here 
+nominatim.global({
+    useragent: "BymeldingPortal",                   // The name of your application 
+    referer: 'http://bymelding.no',                 // The referer link 
+    email: 'ole.petter.haugen@bym.oslo.kommune.no'  // The valid email 
+});
+
+
 
 export class MapView extends React.Component {
     constructor(props) {
         super(props)
+
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick(e) {
+        console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
+
+        var query = {
+            lat: e.latlng.lat,
+            lon: e.latlng.lng
+        };
+
+        nominatim.reverse(query, function (err, data) {
+            if (err) {
+                throw err;
+            }
+
+            console.log(data);
+            console.log(data.display_name);
+        });
+    }
+
     render() {
         return (
             <div>
                 <Map
                     style={{ height: "100vh" }}
                     center={position}
-                    zoom={10}>
+                    zoom={10}
+                    onClick={ (e) => this.handleClick(e)}
+                    >
                     <TileLayer
                         url='https://api.mapbox.com/styles/v1/webforvaltningen/cirlr93tl0010gyly7o56ugi3/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2ViZm9ydmFsdG5pbmdlbiIsImEiOiJjaXJsczQ5dnAwMDMxaG5rd2xnNGt2MGZvIn0.BGtT-dezZ_5hseqXmkMAoQ'
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
