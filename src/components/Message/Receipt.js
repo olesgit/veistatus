0a09d7f4d63db5ab2c60091101cdf77c3dd885c9
Fredrik Folgerø Martinsen <fredrik.martinsen@bouvet.no>
2017-04-04 10:49:18 +0200

    Fixed styling to demo

--- src/components/Message/Receipt.js
+++ src/components/Message/Receipt.js
@@ -13,7 +13,7 @@ class Receipt extends Component {
         const { acknowledge } = this.props;
 
         return (
-            <div>
+            <div className="receipt-content">
                 <h1>Takk for at du gjør byen bedre.</h1>
                 <Button bsStyle="success" onClick={acknowledge}>OK</Button>
             </div>
