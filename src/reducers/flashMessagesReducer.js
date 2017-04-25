66061e2f541c598ed7d39fc84e80a5a0946b2955
Fredrik Folgerø Martinsen <fredrik.martinsen@bouvet.no>
2017-04-25 08:23:29 +0200

    Show flash message on success for reset password

--- src/reducers/flashMessagesReducer.js
+++ src/reducers/flashMessagesReducer.js
@@ -1,29 +1,39 @@
-import { ADD_FLASH_MESSAGE, DELETE_FLASH_MESSAGE } from "../actions/ActionTypes";
-import shortid from "shortid";
-import findIndex from "lodash/findIndex";
+import { RESET_PASSWORD_SUCCESS } from '../actions/loginActions'
+import { ADD_FLASH_MESSAGE, DELETE_FLASH_MESSAGE } from "../actions/ActionTypes"
+import shortid from "shortid"
+import findIndex from "lodash/findIndex"
 
-export default (state = [], action = {}) =>
-{
-    switch (action.type)
-    {
+export default (state = [], action = {}) => {
+    let index;
+    switch (action.type) {
         case ADD_FLASH_MESSAGE:
-            return [
+            return ([
                 {
                     id: shortid.generate(),  // to remove this later from store by id
                     type: action.message.type,
                     text: action.message.text
                 }
-            ];
+            ]);
+
         case DELETE_FLASH_MESSAGE:
-            const index = findIndex(state, { id: action.id });
-            if (index >= 0)
-            {
+            index = findIndex(state, { id: action.id });
+            if (index >= 0) {
                 return [ // to exclude this message from the store
                     ...state.slice(0, index),
                     ...state.slice(index + 1)
                 ];
             }
             return state;
+
+        case RESET_PASSWORD_SUCCESS:
+            return ([
+                {
+                    id: shortid.generate(),
+                    type: 'success',
+                    text: "En epost med link for tilbakestilling av passord er sendt"
+                }
+            ]);
+
         default: return state;
     }
 }
