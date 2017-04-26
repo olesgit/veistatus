05ac470386e9e520f620ae41a59cc1e48a72a1c7
Fredrik Folgerø Martinsen <fredrik.martinsen@bouvet.no>
2017-04-26 15:03:15 +0200

    Added hover-styling to icons in pictures

--- src/components/BilderInput.js
+++ src/components/BilderInput.js
@@ -5,7 +5,9 @@ import Dropzone from 'react-dropzone'
 import './BilderInput.css'
 
 import addIcon from '../images/pluss.svg'
+import addHoverIcon from '../images/pluss-hover.svg'
 import deleteIcon from '../images/slett.svg'
+import deleteHoverIcon from '../images/slett-hover.svg'
 
 const MAX_IMAGE_SIZE = 10485760 //Max size in bytes - 10 MB
 
@@ -22,7 +24,8 @@ class BilderInput extends Component {
         return (
             <div key={file.uuid} className="bilder-preview bilder-thumbnail" style={{ backgroundImage: `url(${file.preview})` }}>
                 <Button onClick={() => this.props.onDelete(file)}>
-                    <Image src={deleteIcon} />
+                    <Image width={25} height={25} className="del" src={deleteIcon} />
+                    <Image width={25} height={25} className="del hover" src={deleteHoverIcon} />
                 </Button>
             </div>
         );
@@ -37,7 +40,8 @@ class BilderInput extends Component {
                 {pictures.length < 4 &&
                     <Dropzone className="bilder-dropzone bilder-thumbnail" onDrop={onDrop} preventDropOnDocument={true} maxSize={MAX_IMAGE_SIZE}>
                         <span className="bilder-dropzone-helper"></span>
-                        <Image src={addIcon} alt='legg til bilde' />
+                        <Image className="add" src={addIcon} alt='legg til bilde' />
+                        <Image className="add hover" src={addHoverIcon} alt='legg til bilde' />
                     </Dropzone>
                 }
             </div>
